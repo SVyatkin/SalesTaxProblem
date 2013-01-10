477b6dd46e70a474d778350bbfe489ff1e61bc38
Sergey Vyatkin <vyatkinhome@yahoo.com>
2013-01-10 09:39:21 -0800

    change to static

--- src/com/decorator/Util.java
+++ src/com/decorator/Util.java
@@ -11,6 +11,19 @@ import java.util.Set;
 public class Util {
 
 	private static Set<String> exemptItems = null;
+	static	{
+		exemptItems = new HashSet<String>();
+		exemptItems.add("book");
+		exemptItems.add("headache pills");
+		exemptItems.add("packet of headache pills");
+		exemptItems.add("box of imported chocolates");
+		exemptItems.add("imported box of chocolates");
+		exemptItems.add("box of chocolates");
+		exemptItems.add("chocolate");
+		exemptItems.add("chocolate bar");
+		exemptItems.add("pills");
+//		exemptItems.add("");
+	}
 	static public double nearest5Percent(double amount) {
 
 		return new BigDecimal(Math.ceil(amount * 20)/20).setScale(2,RoundingMode.HALF_UP).doubleValue();
@@ -25,20 +38,6 @@ public class Util {
 	}
 	
 	public static boolean isExempt(String name) {
-		
-		if (exemptItems == null) {
-			exemptItems = new HashSet<String>();
-			exemptItems.add("book");
-			exemptItems.add("headache pills");
-			exemptItems.add("packet of headache pills");
-			exemptItems.add("box of imported chocolates");
-			exemptItems.add("imported box of chocolates");
-			exemptItems.add("box of chocolates");
-			exemptItems.add("chocolate");
-			exemptItems.add("chocolate bar");
-			exemptItems.add("pills");
-//			exemptItems.add("");
-		}
 		return exemptItems.contains(name);
 	}
 	
